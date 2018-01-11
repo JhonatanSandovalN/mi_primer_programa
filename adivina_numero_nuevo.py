be87b2a3d517e96f8059e46d19bf4ee3caa6a9d4
Jhonatan&Melissa <sjhonatan750@gmail.com>
2018-01-10 23:43:06 -0400

    Añandiendo el archivo test_input

---
+++ adivina_numero_nuevo.py
@@ -0,0 +1,24 @@
+number_to_guess = 5
+
+user_number = int(input("Adivina un numero del 1 al 6: "))
+
+if user_number == number_to_guess:
+    print("Has ganado")
+else:
+    print("Has perdido")
+    user_number = int(input("Intentalo de nuevo: "))
+if user_number == number_to_guess:
+    print("Has ganado")
+else:
+    print("Has perdidoa")
+    user_number = int(input("Intentalo de nuevo: "))
+if user_number == number_to_guess:
+    print("Has ganado")
+else:
+    print("Has perdido")
+    user_number = int(input("Intentalo de nuevo: "))
+if user_number == number_to_guess:
+    print("Has ganado")
+else:
+    print("Has perdido")
+    user_number = int(input("Intentalo de nuevo: "))
